ed20f029ba6fb1d40b7760720c34c70a9f139965
MatteBlackIT <bouwkamp.IT@gmail.com>
2024-08-21 16:03:57 +0900

    Larger step on authentication, next is getting logins stored.

--- medihelp-api/src/main/java/com/medihelp/medihelp/repository/UserRepository.java
+++ medihelp-api/src/main/java/com/medihelp/medihelp/repository/UserRepository.java
@@ -2,6 +2,11 @@ package com.medihelp.medihelp.repository;
 
 import com.medihelp.medihelp.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    User findByUsername(@Param("username") String username);
 }
+
